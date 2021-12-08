182e6014262852da28208a96c25d0dd58b303600
James Moore <j.k.moore@sheffield.ac.uk>
2021-12-08 16:36:55 +0000

    Trim up tag feeds.

--- _plugins/tag_generator.rb
+++ _plugins/tag_generator.rb
@@ -7,14 +7,27 @@ Jekyll::Hooks.register :posts, :post_write do |post|
     tags.each do |tag|
       generate_tag_file(tag) if !all_existing_tags.include?(tag)
     end
+
+    all_existing_feeds = Dir.entries("feeds/tags/")
+      .map { |t| t.match(/(.*).xml/) }
+      .compact.map { |m| m[1] }
+  
+    tags = post['tags'].reject { |t| t.empty? }
+    tags.each do |tag|
+      generate_feed_files(tag) if !all_existing_feeds.include?(tag)
+    end
+
+
   end
   
   def generate_tag_file(tag)
     File.open("_tags/#{tag}.md", "wb") do |file|
     file << "---\nlayout: tags\ntag-name: #{tag}\n---\n"
     end
+  end
 
-    File.open("_feeds/#{tag}.md", "wb") do |file|
+  def generate_feed_files(tag)
+    File.open("feeds/tags/#{tag}.xml", "wb") do |file|
     file << %{---
 layout: feeds
 tag-name: #{tag}
